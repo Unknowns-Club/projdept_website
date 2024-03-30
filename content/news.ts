d6e4741d20c01f350d2b33c645a365aa6594b462
NeterAlex <neteralex@outlook.com>
2024-03-30 13:22:36 +0800

    feat：添加2024考核新闻

--- content/news.ts
+++ content/news.ts
@@ -8,8 +8,17 @@ interface NewsInfo {
 
 export const newsList: NewsInfo[] = [
   {
+    title: "2024年预备队员选拔正式开始",
+    content:
+      "为检验 2024 年新生培训成果及进行 2024 年项目部预备队员选拔工作，进行本次考核。",
+    img: "https://pic.neteralex.cn/iad/2024/03/30/6607a137d988e.webp",
+    time: "2024/3/31",
+    url: "/doc/train/2024_selection_info",
+  },
+  {
     title: "2024年新生培训正式开始",
-    content: "本次培训自 2024 年 3 月 10 日开始，预计持续一个月时间。",
+    content:
+      "欢迎各位同学参加东北农业大学 ACM 集训队项目部 2024 年纳新暨新人培训。",
     img: "https://pic.neteralex.cn/iad/2024/03/24/66002005caa11.webp",
     time: "2024/3/9",
     url: "/doc/train/2024_train_info",
