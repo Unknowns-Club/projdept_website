7e0ccd570fe40e5dd0f711293d2d86d700348b9a
NeterAlex <neteralex@outlook.com>
2024-04-01 14:17:03 +0800

    feat：添加文本内容

---
+++ content/doc-recommend.ts
@@ -0,0 +1,33 @@
+export const docRecommendList: docRecommendItem[] = [
+  {
+    title: "易发音错误的单词",
+    info: "中国程序员容易发音错误的单词列表",
+    category: "杂项",
+    link: "https://github.com/shimohq/chinese-programmer-wrong-pronunciation",
+  },
+  {
+    title: "Web 开发全面教程 - MDN",
+    info: "来自 MDN 的全方面 Web 开发教程",
+    category: "Web",
+    link: "https://developer.mozilla.org/zh-CN/docs/Learn",
+  },
+  {
+    title: "中文技术文档写作规范",
+    info: "写出可读性更高的技术文档应遵守的规范",
+    category: "杂项",
+    link: "https://github.com/ruanyf/document-style-guide",
+  },
+  {
+    title: "开源软件指南",
+    info: "了解如何启动和发展开源项目",
+    category: "项目",
+    link: "https://opensource.guide/zh-hans/",
+  },
+];
+
+interface docRecommendItem {
+  title: string;
+  link: string;
+  info: string;
+  category: string;
+}
