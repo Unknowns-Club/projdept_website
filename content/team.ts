94906d538cf282e58028510ccc2c658e0807a688
NeterAlex <neteralex@outlook.com>
2024-04-16 10:53:11 +0800

    feat：添加预备队员

--- content/team.ts
+++ content/team.ts
@@ -23,7 +23,30 @@ const fullMembers: MemberInfo[] = [
   { name: "刘来熙", clazz: "计科2205" },
 ];
 
-const reservedMembers: MemberInfo[] = [];
+const reservedMembers: MemberInfo[] = [
+  { name: "何济宏", clazz: "计科2204" },
+  { name: "梁思琪", clazz: "计科2205" },
+  { name: "林思宇", clazz: "微机2310" },
+  { name: "王佳", clazz: "行政2302" },
+  { name: "徐观昌", clazz: "微机2303" },
+  { name: "沙峻", clazz: "微机2310" },
+  { name: "张博伦", clazz: "微机2310" },
+  { name: "车丽钠", clazz: "微机2305" },
+  { name: "陈帅", clazz: "微机2305" },
+  { name: "赵羽诗", clazz: "微机2302" },
+  { name: "王泽旭", clazz: "微机2306" },
+  { name: "赵鸿博", clazz: "智能2202" },
+  { name: "鲁思雨", clazz: "数据2302" },
+  { name: "冯美琪", clazz: "软件2201" },
+  { name: "曾艺豪", clazz: "微机2306" },
+  { name: "刘帅", clazz: "计科2204" },
+  { name: "潘玉凤", clazz: "微机2306" },
+  { name: "宋伟枫", clazz: "微机2306" },
+  { name: "于若男", clazz: "微机2304" },
+  { name: "尚钰哲", clazz: "微机2309" },
+  { name: "张连博", clazz: "工二2302" },
+  { name: "张宏达", clazz: "微机2309" },
+];
 
 export const teamList = {
   studentCoaches,
