d39cbf142d1571827c7b00565fb28c551bbae16e
NeterAlex <neteralex@outlook.com>
2024-08-12 19:57:17 +0800

    style: 重设格式

--- components/shared/BookCard.tsx
+++ components/shared/BookCard.tsx
@@ -1,72 +1,72 @@
-import { Box, css, Flex, HStack, Text, VStack } from "@kuma-ui/core";
-import { docRecommendList } from "../../content/doc-recommend";
+import {Box, css, Flex, HStack, Text, VStack} from "@kuma-ui/core";
+import {docRecommendList} from "../../content/doc-recommend";
 
 interface BookCardParams {
-  title: string;
-  link: string;
-  info: string;
-  category: string;
+    title: string;
+    link: string;
+    info: string;
+    category: string;
 }
 
 export const BookCardList = () => {
-  return (
-    <Flex py={"1em"} flexWrap="wrap" width={"100%"} gap={"1em"}>
-      {docRecommendList.map((it, index) => {
-        return (
-          <BookCard
-            key={index}
-            link={it.link}
-            title={it.title}
-            info={it.info}
-            category={it.category}
-          />
-        );
-      })}
-    </Flex>
-  );
+    return (
+        <Flex py={"1em"} flexWrap="wrap" width={"100%"} gap={"1em"}>
+            {docRecommendList.map((it, index) => {
+                return (
+                    <BookCard
+                        key={index}
+                        link={it.link}
+                        title={it.title}
+                        info={it.info}
+                        category={it.category}
+                    />
+                );
+            })}
+        </Flex>
+    );
 };
 
-export const BookCard = ({ title, link, info, category }: BookCardParams) => {
-  return (
-    <Box
-      className={css`
-        height: 200px;
-        width: 150px;
-        border-radius: 10px;
-        background: var(--shiki-color-background);
-        box-shadow: var(--shiki-shadow) 0 1px 3px 1px;
-        padding: 1em;
-        cursor: pointer;
-        transition: transform 1s;
-      `}
-      _hover={{ transform: "scale(1.02)" }}
-      onClick={() => window.open(link, "_blank")}
-    >
-      <Flex
-        h={"100%"}
-        flexDirection={"column"}
-        justifyContent={"space-between"}
-      >
-        <VStack gap="0.5em">
-          <Text fontWeight="bold" color="var(--shiki-token-constant)">
-            {title}
-          </Text>
-          <Text fontSize={"0.9em"} color={"var(--shiki-color-text: #d1d1d1)"}>
-            {info}
-          </Text>
-        </VStack>
-        <HStack>
-          <Box
-            borderRadius={10}
-            fontSize={"0.8em"}
-            px={"0.5em"}
-            py={"0.125em"}
-            bg={"var(--shiki-token-constant-lighter)"}
-          >
-            {category}
-          </Box>
-        </HStack>
-      </Flex>
-    </Box>
-  );
+export const BookCard = ({title, link, info, category}: BookCardParams) => {
+    return (
+        <Box
+            className={css`
+                height: 200px;
+                width: 150px;
+                border-radius: 10px;
+                background: var(--shiki-color-background);
+                box-shadow: var(--shiki-shadow) 0 1px 3px 1px;
+                padding: 1em;
+                cursor: pointer;
+                transition: transform 1s;
+            `}
+            _hover={{transform: "scale(1.02)"}}
+            onClick={() => window.open(link, "_blank")}
+        >
+            <Flex
+                h={"100%"}
+                flexDirection={"column"}
+                justifyContent={"space-between"}
+            >
+                <VStack gap="0.5em">
+                    <Text fontWeight="bold" color="var(--shiki-token-constant)">
+                        {title}
+                    </Text>
+                    <Text fontSize={"0.9em"} color={"var(--shiki-color-text: #d1d1d1)"}>
+                        {info}
+                    </Text>
+                </VStack>
+                <HStack>
+                    <Box
+                        borderRadius={10}
+                        fontSize={"0.8em"}
+                        px={"0.5em"}
+                        py={"0.125em"}
+                        bg={"var(--shiki-token-constant-lighter)"}
+                    >
+                        {category}
+                    </Box>
+                </HStack>
+            </Flex>
+        </Box>
+    );
 };
