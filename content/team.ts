ed82929d82dccad0e1e6c2250d004e19d5253dda
NeterAlex <neteralex@outlook.com>
2024-08-12 19:57:42 +0800

    feat: 新增退役队员列表，并更新成员

--- content/team.ts
+++ content/team.ts
@@ -1,57 +1,51 @@
 interface MemberInfo {
-  clazz: string;
-  name: string;
+    clazz: string;
+    name: string;
 }
 
 const studentCoaches: MemberInfo[] = [
-  { name: "李子豪", clazz: "微机2004" },
-  { name: "霍欣彤", clazz: "软件2003" },
+    {name: "盖永瑞", clazz: "微机2108"},
+    {name: "闫志涛", clazz: "微机2107"},
 ];
 
 const teamLeader: MemberInfo = {
-  name: "陈玟杏",
-  clazz: "计科2104",
+    name: "韩玉坤",
+    clazz: "物联2202",
 };
 
 const teamAssistLeaders: MemberInfo[] = [
-  { name: "韩玉坤", clazz: "物联2202" },
-  { name: "杨博宇", clazz: "计科2205" },
+    {name: "杨博宇", clazz: "计科2205"},
+    {name: "王泽旭", clazz: "微机2306"},
+    {name: "车丽钠", clazz: "微机2305"},
+    {name: "张博伦", clazz: "微机2310"},
 ];
 
 const fullMembers: MemberInfo[] = [
-  { name: "李俊翔", clazz: "微机2102" },
-  { name: "刘来熙", clazz: "计科2205" },
+    {name: "刘来熙", clazz: "计科2205"},
+    {name: "刘帅", clazz: "计科2204"},
+    {name: "赵鸿博", clazz: "智能2202"},
+    {name: "于若男", clazz: "微机2304"},
+    {name: "张宏达", clazz: "微机2309"},
+    {name: "徐观昌", clazz: "微机2303"},
+    {name: "曾艺豪", clazz: "微机2306"},
+    {name: "潘玉凤", clazz: "微机2306"},
+    {name: "赵羽诗", clazz: "微机2302"},
 ];
 
-const reservedMembers: MemberInfo[] = [
-  { name: "何济宏", clazz: "计科2204" },
-  { name: "梁思琪", clazz: "计科2205" },
-  { name: "林思宇", clazz: "微机2310" },
-  { name: "王佳", clazz: "行政2302" },
-  { name: "徐观昌", clazz: "微机2303" },
-  { name: "沙峻", clazz: "微机2310" },
-  { name: "张博伦", clazz: "微机2310" },
-  { name: "车丽钠", clazz: "微机2305" },
-  { name: "陈帅", clazz: "微机2305" },
-  { name: "赵羽诗", clazz: "微机2302" },
-  { name: "王泽旭", clazz: "微机2306" },
-  { name: "赵鸿博", clazz: "智能2202" },
-  { name: "鲁思雨", clazz: "数据2302" },
-  { name: "冯美琪", clazz: "软件2201" },
-  { name: "曾艺豪", clazz: "微机2306" },
-  { name: "刘帅", clazz: "计科2204" },
-  { name: "潘玉凤", clazz: "微机2306" },
-  { name: "宋伟枫", clazz: "微机2306" },
-  { name: "于若男", clazz: "微机2304" },
-  { name: "尚钰哲", clazz: "微机2309" },
-  { name: "张连博", clazz: "工二2302" },
-  { name: "张宏达", clazz: "微机2309" },
-];
+const reservedMembers: MemberInfo[] = [];
+
+const retiredMembers: MemberInfo[] = [
+    {name: "李俊翔", clazz: "微机2102"},
+    {name: "陈玟杏", clazz: "微机2103"},
+    {name: "黎俊威", clazz: "微机2111"},
+    {name: "张晓斌", clazz: "微机2107"},
+]
 
 export const teamList = {
-  studentCoaches,
-  teamLeader,
-  teamAssistLeaders,
-  fullMembers,
-  reservedMembers,
+    studentCoaches,
+    teamLeader,
+    teamAssistLeaders,
+    fullMembers,
+    reservedMembers,
+    retiredMembers
 };
